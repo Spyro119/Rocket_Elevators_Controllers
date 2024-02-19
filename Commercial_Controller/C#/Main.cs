ded8dc74d9b7156d0709bf2ec112ac4a1a7e490e
Samuel Jubinville <samueljubinville119@gmail.com>
2024-02-19 17:02:40 -0500

    refactored code and added documentation

--- Commercial_Controller/C#/Main.cs
+++ Commercial_Controller/C#/Main.cs
@@ -1,148 +1,177 @@
 ﻿using System;
-using Class;
+using Rocket_Elevators_Commercial_Controller.Commercial_Controller.Classes;
 
-namespace Rocket_Elevators_Commercial_Controller
+namespace Rocket_Elevators_Commercial_Controller.Commercial_Controller
 {
-    class Program
+    internal class Program
     {
-        static void Main(){
-        
-        var elevatorNumbersTotal = 20;
-	    var floorNumbersTotal = 60;
-	    var numberOfBasement = 6;
-	    var numberOfColumn = 4;
-        //                                *****CREATING BATTERY ******
-        Battery battery = new Battery(elevatorNumbersTotal,numberOfColumn,floorNumbersTotal,numberOfBasement);
+        static void Main()
+        {
+            var elevatorNumbersTotal = 20;
+            var floorNumbersTotal = 60;
+            var numberOfBasement = 6;
+            var numberOfColumn = 4;
+            var program = new Program();
 
-        //                                      *****SCENARIOS INPUTS*****
+            //                                *****CREATING BATTERY ******
+            Battery battery = new Battery(elevatorNumbersTotal, numberOfColumn, floorNumbersTotal, numberOfBasement);
 
-        Console.WriteLine("Please choose a scenario: 1, 2, 3 or 4");
-        string scenario = Console.ReadLine();
-        while (scenario != "1" && scenario != "2" && scenario != "3" && scenario != "4"){
-        Console.WriteLine("Please choose a scenario: 1, 2, 3 or 4");
-        scenario = Console.ReadLine();
+            Console.WriteLine("Please choose a scenario: 1, 2, 3 or 4");
+            int.TryParse(Console.ReadLine(), out int scenario);
+            while (scenario <= 0 || scenario > 4)
+            {
+                Console.WriteLine("Please choose a scenario: 1, 2, 3 or 4");
+                int.TryParse(Console.ReadLine(), out scenario);
+            }
+            switch (scenario)
+            {
+                case 1:
+                    program.scenario_one(battery);
+                    break;
+                case 2:
+                    program.scenario_two(battery);
+                    break;
+                case 3:
+                    program.scenario_three(battery);
+                    break;
+                case 4:
+                    program.scenario_four(battery);
+                    break;
+                default:
+                    break;
+            }
+
+            program.Retry();
         }
-        if (scenario == "1"){
-          //Elevator B1 at 20th Floor, going "down" to 5th
-		battery.columnList[1].elevatorList[0].currentFloor = 20;
-		battery.columnList[1].elevatorList[0].direction = "down";
 
-		//Elevator B2 at 3rd, going "up" to 15th
-		battery.columnList[1].elevatorList[1].currentFloor = 3;
-		battery.columnList[1].elevatorList[1].direction = "up";
-
-		//Elevator B3 at 13th, going "down" to 1st
-		battery.columnList[1].elevatorList[2].currentFloor = 13;
-		battery.columnList[1].elevatorList[2].direction = "down";
-
-		//Elevator B4 at 15th, going "down" to 2nd
-		battery.columnList[1].elevatorList[3].currentFloor = 15;
-		battery.columnList[1].elevatorList[3].direction = "down";
-
-		//Elevator B5 at 6th, going "down" to 1st
-		battery.columnList[1].elevatorList[4].currentFloor = 6;
-		battery.columnList[1].elevatorList[4].direction = "down";
-
-		//1st, someone is at 1st floor and requests the 20th floor
-		var requestedFloor = 20;
-		battery.assignElevator(requestedFloor);
-
- } else if(scenario == "2"){
-          //Column C
-		//Elevator C1 at 1ts Floor, going "up" to 21st
-		battery.columnList[2].elevatorList[0].currentFloor = 1;
-		battery.columnList[2].elevatorList[0].direction = "up";
-
-		//Elevator C2 at 23rd, going "up" to 28th
-		battery.columnList[2].elevatorList[1].currentFloor = 23;
-		battery.columnList[2].elevatorList[1].direction = "up";
-
-		//Elevator C3 at 33th, going "down" to 1st
-		battery.columnList[2].elevatorList[2].currentFloor = 33;
-		battery.columnList[2].elevatorList[2].direction = "down";
-
-		//Elevator C4 at 40th, going "down" to 24th
-		battery.columnList[2].elevatorList[3].currentFloor = 40;
-		battery.columnList[2].elevatorList[3].direction = "down";
-
-		//Elevator C5 at 39th, going "down" to 1st
-		battery.columnList[2].elevatorList[4].currentFloor = 39;
-		battery.columnList[2].elevatorList[4].direction = "down";
-
-		//1st, someone is at 1st floor and requests the 36th floor, elevator C1 is expected to be sent
-		var requestedFloor = 36;
-		battery.assignElevator(requestedFloor);
-
-   }else if (scenario == "3") {
-		//Column D
-		//Elevator D1 at 58th Floor, going "down" to 1st
-		battery.columnList[3].elevatorList[0].currentFloor = 58;
-		battery.columnList[3].elevatorList[0].direction = "down";
-
-		//Elevator D2 at 50th, going "up" to 60th
-		battery.columnList[3].elevatorList[1].currentFloor = 50;
-		battery.columnList[3].elevatorList[1].direction = "up";
-
-		//Elevator D3 at 46th, going "up" to 58th
-		battery.columnList[3].elevatorList[2].currentFloor = 46;
-		battery.columnList[3].elevatorList[2].direction = "up";
-
-		//Elevator D4 at 1st, going "down" to 54th
-		battery.columnList[3].elevatorList[3].currentFloor = 1;
-		battery.columnList[3].elevatorList[3].direction = "up";
-
-		//Elevator D5 at 60th, going "down" to 1st
-		battery.columnList[3].elevatorList[4].currentFloor = 60;
-		battery.columnList[3].elevatorList[4].direction = "down";
-
-		//1st, someone is at 54th floor and requests the 1st floor, elevator D1 is expected to be sent
-		var floorNumber = 54;
-		battery.requestElevator(floorNumber);
-
-	} else if (scenario == "4") {
-		//Column A
-		//Elevator A1 at B4th Floor, Being "idle"
-		battery.columnList[0].elevatorList[0].currentFloor = -4;
-		battery.columnList[0].elevatorList[0].direction = "idle";
-
-		//Elevator A2 "idle" at 1st Floor
-		battery.columnList[0].elevatorList[1].currentFloor = 1;
-		battery.columnList[0].elevatorList[1].direction = "idle";
-
-		//Elevator A3 at B3rd, going "down" to B5th
-		battery.columnList[0].elevatorList[2].currentFloor = -3;
-		battery.columnList[0].elevatorList[2].direction = "down";
-
-		//Elevator A4 at B6th, going "up" to 1st
-		battery.columnList[0].elevatorList[3].currentFloor = -6;
-		battery.columnList[0].elevatorList[3].direction = "up";
-
-		//Elevator A5 at B1st, going "down" to b6th
-		battery.columnList[0].elevatorList[4].currentFloor = -1;
-		battery.columnList[0].elevatorList[4].direction = "down";
-
-		//1st, someone is at B3rd floor and requests the 1st floor, elevator A4 is expected to be sent
-		var numberFloor = -3;
-		battery.requestElevator(numberFloor);
-
-	}
-
-     void retry(){
-      Console.WriteLine("Want to retry? y/n");
-      string retryInput = Console.ReadLine();
-        while (retryInput != "y" && retryInput != "n"){
-          Console.WriteLine("Please choose between y for Yes or n for no.");
-          retryInput = Console.ReadLine();
+        void Retry()
+        {
+            Console.WriteLine("Want to retry? y/n");
+            string retryInput = Console.ReadLine();
+            while (retryInput != "y" && retryInput != "n")
+            {
+                Console.WriteLine("Please choose between y for Yes or n for no.");
+                retryInput = Console.ReadLine();
+            }
+            if (retryInput == "y")
+            {
+                Main();
+            }
+            else if (retryInput == "n")
+                Console.WriteLine("Thank you. Exiting program.");
         }
-        if (retryInput == "y"){
-          Main();
-        } else if(retryInput == "n")
-        Console.WriteLine("Thank you. Exiting program.");
-    }
-	retry(); 
-        
-              
-    }
+
+
+        public void scenario_one(Battery battery)
+        {
+            //Elevator B1 at 20th Floor, going "Down" to 5th
+            battery.ColumnList[1].ElevatorList[0].CurrentFloor = 20;
+            battery.ColumnList[1].ElevatorList[0].Direction = "Down";
+
+            //Elevator B2 at 3rd, going "Up" to 15th
+            battery.ColumnList[1].ElevatorList[1].CurrentFloor = 3;
+            battery.ColumnList[1].ElevatorList[1].Direction = "Up";
+
+            //Elevator B3 at 13th, going "Down" to 1st
+            battery.ColumnList[1].ElevatorList[2].CurrentFloor = 13;
+            battery.ColumnList[1].ElevatorList[2].Direction = "Down";
+
+            //Elevator B4 at 15th, going "Down" to 2nd
+            battery.ColumnList[1].ElevatorList[3].CurrentFloor = 15;
+            battery.ColumnList[1].ElevatorList[3].Direction = "Down";
+
+            //Elevator B5 at 6th, going "Down" to 1st
+            battery.ColumnList[1].ElevatorList[4].CurrentFloor = 6;
+            battery.ColumnList[1].ElevatorList[4].Direction = "Down";
+
+            //1st, someone is at 1st floor and requests the 20th floor
+            var requestedFloor = 20;
+            battery.AssignElevator(requestedFloor);
+        }
+
+        public void scenario_two(Battery battery)
+        {
+            //Column C
+            //Elevator C1 at 1ts Floor, going "Up" to 21st
+            battery.ColumnList[2].ElevatorList[0].CurrentFloor = 1;
+            battery.ColumnList[2].ElevatorList[0].Direction = "Up";
+
+            //Elevator C2 at 23rd, going "Up" to 28th
+            battery.ColumnList[2].ElevatorList[1].CurrentFloor = 23;
+            battery.ColumnList[2].ElevatorList[1].Direction = "Up";
+
+            //Elevator C3 at 33th, going "Down" to 1st
+            battery.ColumnList[2].ElevatorList[2].CurrentFloor = 33;
+            battery.ColumnList[2].ElevatorList[2].Direction = "Down";
+
+            //Elevator C4 at 40th, going "Down" to 24th
+            battery.ColumnList[2].ElevatorList[3].CurrentFloor = 40;
+            battery.ColumnList[2].ElevatorList[3].Direction = "Down";
+
+            //Elevator C5 at 39th, going "Down" to 1st
+            battery.ColumnList[2].ElevatorList[4].CurrentFloor = 39;
+            battery.ColumnList[2].ElevatorList[4].Direction = "Down";
+
+            //1st, someone is at 1st floor and requests the 36th floor, elevator 3-1 is expected to be sent
+            var requestedFloor = 36;
+            battery.AssignElevator(requestedFloor);
+        }
+
+        public void scenario_three(Battery battery)
+        {
+            //Column D
+            //Elevator D1 at 58th Floor, going "Down" to 1st
+            battery.ColumnList[3].ElevatorList[0].CurrentFloor = 58;
+            battery.ColumnList[3].ElevatorList[0].Direction = "Down";
+
+            //Elevator D2 at 50th, going "Up" to 60th
+            battery.ColumnList[3].ElevatorList[1].CurrentFloor = 50;
+            battery.ColumnList[3].ElevatorList[1].Direction = "Up";
+
+            //Elevator D3 at 46th, going "Up" to 58th
+            battery.ColumnList[3].ElevatorList[2].CurrentFloor = 46;
+            battery.ColumnList[3].ElevatorList[2].Direction = "Up";
+
+            //Elevator D4 at 1st, going "Down" to 54th
+            battery.ColumnList[3].ElevatorList[3].CurrentFloor = 1;
+            battery.ColumnList[3].ElevatorList[3].Direction = "Up";
+
+            //Elevator D5 at 60th, going "Down" to 1st
+            battery.ColumnList[3].ElevatorList[4].CurrentFloor = 60;
+            battery.ColumnList[3].ElevatorList[4].Direction = "Down";
+
+            //1st, someone is at 54th floor and requests the 1st floor, elevator 4-1 is expected to be sent
+            var floorNumber = 54;
+            battery.RequestElevator(floorNumber);
+
+        }
+
+        public void scenario_four(Battery battery)
+        {
+            //Column A
+            //Elevator A1 at B4th Floor, Being "Idle"
+            battery.ColumnList[0].ElevatorList[0].CurrentFloor = -4;
+            battery.ColumnList[0].ElevatorList[0].Direction = "Idle";
+
+            //Elevator A2 "Idle" at 1st Floor
+            battery.ColumnList[0].ElevatorList[1].CurrentFloor = 1;
+            battery.ColumnList[0].ElevatorList[1].Direction = "Idle";
+
+            //Elevator A3 at B3rd, going "Down" to B5th
+            battery.ColumnList[0].ElevatorList[2].CurrentFloor = -3;
+            battery.ColumnList[0].ElevatorList[2].Direction = "Down";
+
+            //Elevator A4 at B6th, going "Up" to 1st
+            battery.ColumnList[0].ElevatorList[3].CurrentFloor = -6;
+            battery.ColumnList[0].ElevatorList[3].Direction = "Up";
+
+            //Elevator A5 at B1st, going "Down" to b6th
+            battery.ColumnList[0].ElevatorList[4].CurrentFloor = -1;
+            battery.ColumnList[0].ElevatorList[4].Direction = "Down";
+
+            //1st, someone is at B3rd floor and requests the 1st floor, elevator 1-4 is expected to be sent
+            var numberFloor = -3;
+            battery.RequestElevator(numberFloor);
+        }
     }
 }
-
--- Commercial_Controller/C#/class/Battery.cs
+++ Commercial_Controller/C#/class/Battery.cs
@@ -1,93 +1,84 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Class {
-
-public class Battery {
-  public int numberOfColumn;
-  public int floorNumbersTotal;
-  public List<Column> columnList;
-
-  public Battery(int elevatorNumbersTotal, int numberOfColumn, int floorNumbersTotal, int numberOfBasement) {
-    this.numberOfColumn = numberOfColumn;
-    this.floorNumbersTotal = floorNumbersTotal;
-    this.columnList = new List<Column>(); 
-    var numberOfElevators = elevatorNumbersTotal / numberOfColumn;
-    var numberOfFloors = floorNumbersTotal / (numberOfColumn - 1);
-
-    // *****GENERATING COLUMNS AND ELEVATORS FOR EACH COLUMNS*****
-    for (var i = 1; i <= numberOfColumn; i++){
-      var col = new Column("Column " + i, numberOfElevators, numberOfFloors);
-      columnList.Add(col);
-
-      for (var j =1; j <= numberOfElevators; j++){
-        var elev = new Elevator("Elevator " + i + "." + j, 1, "idle", 1);
-        col.elevatorList.Add(elev);
-      }
-    }
-    
-    for (var i = 0; i < numberOfColumn; i++ ){
-      for (var k = 1; k <= numberOfFloors; k++){
-        if (i == 0){
-          if (k == 1) {
-            // *****ADDING RC TO COLUMN DEDICATED TO BASEMENT *****
-            columnList[i].floorList.Add(k);
-          }
-          if (k <= numberOfBasement) {
-            //                                     ***** BASEMENT FLOOR LIST TO COLUMN 0 *****
-            columnList[i].floorList.Add(-k);
-          }
-        } else if (i == 1){
-          //                                            ***** FLOOR 1 TO 20 FOR 1ST COLUMN *****
-          columnList[i].floorList.Add(k);
-        } else {
-          //                            ***** ADD RC TO EVERY COLUMN THAT ISN'T IN INDEX 0 AND 1 *****
-          if (k == 1) {
-            columnList[i].floorList.Add(1);
-          }
-          //       ***** ADDING FLOORS TO EVERY OTHER COLUMN, FloorsPerColumn BEING THE RANGE OF FLOORS PER COLUMN *****
-
-          columnList[i].floorList.Add( k+((i-1)*numberOfFloors));
-
-
-          }
-        }
-    }
-    }
- public void requestElevator(int floorNumber){
-      if (floorNumber != 1){
-        int requestedFloor = 1;
-        findColumn(floorNumber, requestedFloor);
-    
-      } 
-}
-    public void assignElevator(int requestedFloor){
-      if (requestedFloor != 1){
-       int floorNumber = 1;
-      findColumn(floorNumber, requestedFloor);
-    } 
-
-    
-
-}
-    Column findColumn (int floorNumber, int requestedFloor){
-      Column bestCol = this.columnList[0];
-      for (var i =0; i < this.columnList.Count; i++)
-      {
-		foreach (int k in this.columnList[i].floorList)
+namespace Rocket_Elevators_Commercial_Controller.Commercial_Controller.Classes
+{
+    internal class Battery
     {
-			if (floorNumber == 1 && requestedFloor == k){
-				bestCol = this.columnList[i];
-			} else if (floorNumber == k && requestedFloor == 1){
-				bestCol = this.columnList[i];
-				
-			}
-		}
-	}
-    
-	    bestCol.findElevator(floorNumber, requestedFloor, bestCol);
-        
-        return bestCol;
+        public int ColumnCount;
+        public int FloorsCount;
+        public List<Column> ColumnList = new List<Column>();
+        public Column SelectedColumn = null;
+
+        public Battery(int elevatorsCount, int columnCount, int floorsCount, int basementFloorCount)
+        {
+            this.ColumnCount = columnCount;
+            this.FloorsCount = floorsCount;
+            var numberOfElevators = elevatorsCount / this.ColumnCount;
+            int numberOfFloors = this.FloorsCount / (this.ColumnCount - 1);
+
+            for (var i = 1; i <= columnCount; i++)
+            {
+                Column column = new Column("Column " + i);
+                this.ColumnList.Add(column);
+                    for (var j = 1; j <= numberOfElevators; j++)
+                {
+                    column.AddElevator(new Elevator("Elevator " + i + "-" + j, 1, "Idle"));
+                }
+            }
+
+            for (var i = 0; i < columnCount; i++)
+            {
+                if (i == 0)
+                {
+                    this.ColumnList[i].FloorsMin = -basementFloorCount;
+                    this.ColumnList[i].FloorsMax = 1;
+                }
+                else
+                {
+                    this.ColumnList[i].FloorsMin = (i - 1) * numberOfFloors + 1;
+                    this.ColumnList[i].FloorsMax = i * numberOfFloors;
+                }
+            }
+        }
+
+        public void RequestElevator(int floorNumber)
+        {
+            if (floorNumber != 1)
+            {
+                int requestedFloor = 1;
+                this.FindColumn(floorNumber, requestedFloor);
+
+            }
+        }
+        public void AssignElevator(int requestedFloor)
+        {
+            if (requestedFloor != 1)
+            {
+                int floorNumber = 1;
+                this.FindColumn(floorNumber, requestedFloor);
+            }
+        }
+
+
+        Column FindColumn(int requestedFloor, int destinationFloor)
+        {
+            if (destinationFloor < 0) this.SelectedColumn = this.ColumnList[0];
+            else
+            {
+                for (var i = 0; i < this.ColumnList.Count; i++)
+                {
+                    if (requestedFloor == 1 && destinationFloor <= this.ColumnList[i].FloorsMax && destinationFloor >= this.ColumnList[i].FloorsMin) this.SelectedColumn = this.ColumnList[i];
+                    else if (requestedFloor <= this.ColumnList[i].FloorsMax && requestedFloor >= this.ColumnList[i].FloorsMin && destinationFloor == 1) this.SelectedColumn = this.ColumnList[i];
+                }
+            }
+            
+            if (this.SelectedColumn == null) throw new Exception("Aucune colonne n'a été sélectionné.");
+            this.SelectedColumn.FindElevator(requestedFloor, destinationFloor);
+            return this.SelectedColumn;
         }
     }
 }
