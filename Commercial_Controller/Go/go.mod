ba6065fe98e52e8d7bea867f1a075d6c1dd2305a
Samuel Jubinville <samueljubinville119@gmail.com>
2024-02-19 18:16:24 -0500

    Restructured project and initialised go.mod

---
+++ Commercial_Controller/Go/go.mod
@@ -0,0 +1,3 @@
+module github.com/Spyro119/Rocket_Elevators_Controllers
+
+go 1.21.1
--- Commercial_Controller/Go/src/main.go
+++ Commercial_Controller/Go/src/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Spyro119/Rocket_Elevators_Controllers/classes"
+)
 
 //																	JUST RUN PROGRAMM FOR SCENARIOS
 //																	INTERACTIVE INPUT ALREADY IN PLACE
@@ -21,7 +25,6 @@ func retry() {
 
 }
 
-
 func main() {
 	//                                            *****VARIABLES*****
 	var elevatorNumbersTotal = 20
@@ -29,7 +32,7 @@ func main() {
 	var numberOfBasement = 6
 	var numberColumn = 4
 
-	b := newBattery(elevatorNumbersTotal, numberFloorsTotal, numberColumn, numberOfBasement)
+	b := classes.NewBattery(elevatorNumbersTotal, numberFloorsTotal, numberColumn, numberOfBasement)
 
 	//                                           *****SCENARIOS INPUT****
 
@@ -43,28 +46,28 @@ func main() {
 	if scenario == 1 {
 		//Column B
 		//Elevator B1 at 20th Floor, going "down" to 5th
-		b.columnList[1].elevatorList[0].currentFloor = 20
-		b.columnList[1].elevatorList[0].direction = "down"
+		b.ColumnList[1].ElevatorList[0].CurrentFloor = 20
+		b.ColumnList[1].ElevatorList[0].Direction = "down"
 
 		//Elevator B2 at 3rd, going "up" to 15th
-		b.columnList[1].elevatorList[1].currentFloor = 3
-		b.columnList[1].elevatorList[1].direction = "up"
+		b.ColumnList[1].ElevatorList[1].CurrentFloor = 3
+		b.ColumnList[1].ElevatorList[1].Direction = "up"
 
 		//Elevator B3 at 13th, going "down" to 1st
-		b.columnList[1].elevatorList[2].currentFloor = 13
-		b.columnList[1].elevatorList[2].direction = "down"
+		b.ColumnList[1].ElevatorList[2].CurrentFloor = 13
+		b.ColumnList[1].ElevatorList[2].Direction = "down"
 
 		//Elevator B4 at 15th, going "down" to 2nd
-		b.columnList[1].elevatorList[3].currentFloor = 15
-		b.columnList[1].elevatorList[3].direction = "down"
+		b.ColumnList[1].ElevatorList[3].CurrentFloor = 15
+		b.ColumnList[1].ElevatorList[3].Direction = "down"
 
 		//Elevator B5 at 6th, going "down" to 1st
-		b.columnList[1].elevatorList[4].currentFloor = 6
-		b.columnList[1].elevatorList[4].direction = "down"
+		b.ColumnList[1].ElevatorList[4].CurrentFloor = 6
+		b.ColumnList[1].ElevatorList[4].Direction = "down"
 
 		//1st, someone is at 1st floor and requests the 20th floor
 		var requestedFloor = 20
-		b.assignElevator(requestedFloor)
+		b.AssignElevator(requestedFloor)
 
 		//, elevator B5 is expected to be sent in Scenario 1
 
@@ -72,80 +75,80 @@ func main() {
 
 		//Column C
 		//Elevator C1 at 1ts Floor, going "up" to 21st
-		b.columnList[2].elevatorList[0].currentFloor = 1
-		b.columnList[2].elevatorList[0].direction = "up"
+		b.ColumnList[2].ElevatorList[0].CurrentFloor = 1
+		b.ColumnList[2].ElevatorList[0].Direction = "up"
 
 		//Elevator C2 at 23rd, going "up" to 28th
-		b.columnList[2].elevatorList[1].currentFloor = 23
-		b.columnList[2].elevatorList[1].direction = "up"
+		b.ColumnList[2].ElevatorList[1].CurrentFloor = 23
+		b.ColumnList[2].ElevatorList[1].Direction = "up"
 
 		//Elevator C3 at 33th, going "down" to 1st
-		b.columnList[2].elevatorList[2].currentFloor = 33
-		b.columnList[2].elevatorList[2].direction = "down"
+		b.ColumnList[2].ElevatorList[2].CurrentFloor = 33
+		b.ColumnList[2].ElevatorList[2].Direction = "down"
 
 		//Elevator C4 at 40th, going "down" to 24th
-		b.columnList[2].elevatorList[3].currentFloor = 40
-		b.columnList[2].elevatorList[3].direction = "down"
+		b.ColumnList[2].ElevatorList[3].CurrentFloor = 40
+		b.ColumnList[2].ElevatorList[3].Direction = "down"
 
 		//Elevator C5 at 39th, going "down" to 1st
-		b.columnList[2].elevatorList[4].currentFloor = 39
-		b.columnList[2].elevatorList[4].direction = "down"
+		b.ColumnList[2].ElevatorList[4].CurrentFloor = 39
+		b.ColumnList[2].ElevatorList[4].Direction = "down"
 
 		//1st, someone is at 1st floor and requests the 36th floor, elevator C1 is expected to be sent
 		var requestedFloor = 36
-		b.assignElevator(requestedFloor)
+		b.AssignElevator(requestedFloor)
 
 	} else if scenario == 3 {
 		//Column D
 		//Elevator D1 at 58th Floor, going "down" to 1st
-		b.columnList[3].elevatorList[0].currentFloor = 58
-		b.columnList[3].elevatorList[0].direction = "down"
+		b.ColumnList[3].ElevatorList[0].CurrentFloor = 58
+		b.ColumnList[3].ElevatorList[0].Direction = "down"
 
 		//Elevator D2 at 50th, going "up" to 60th
-		b.columnList[3].elevatorList[1].currentFloor = 50
-		b.columnList[3].elevatorList[1].direction = "up"
+		b.ColumnList[3].ElevatorList[1].CurrentFloor = 50
+		b.ColumnList[3].ElevatorList[1].Direction = "up"
 
 		//Elevator D3 at 46th, going "up" to 58th
-		b.columnList[3].elevatorList[2].currentFloor = 46
-		b.columnList[3].elevatorList[2].direction = "up"
+		b.ColumnList[3].ElevatorList[2].CurrentFloor = 46
+		b.ColumnList[3].ElevatorList[2].Direction = "up"
 
 		//Elevator D4 at 1st, going "down" to 54th
-		b.columnList[3].elevatorList[3].currentFloor = 1
-		b.columnList[3].elevatorList[3].direction = "up"
+		b.ColumnList[3].ElevatorList[3].CurrentFloor = 1
+		b.ColumnList[3].ElevatorList[3].Direction = "up"
 
 		//Elevator D5 at 60th, going "down" to 1st
-		b.columnList[3].elevatorList[4].currentFloor = 60
-		b.columnList[3].elevatorList[4].direction = "down"
+		b.ColumnList[3].ElevatorList[4].CurrentFloor = 60
+		b.ColumnList[3].ElevatorList[4].Direction = "down"
 
 		//1st, someone is at 54th floor and requests the 1st floor, elevator D1 is expected to be sent
 		var floorNumber = 54
-		b.requestElevator(floorNumber)
+		b.RequestElevator(floorNumber)
 
 	} else if scenario == 4 {
 		//Column A
 		//Elevator A1 at B4th Floor, Being "idle"
-		b.columnList[0].elevatorList[0].currentFloor = -4
-		b.columnList[0].elevatorList[0].direction = "idle"
+		b.ColumnList[0].ElevatorList[0].CurrentFloor = -4
+		b.ColumnList[0].ElevatorList[0].Direction = "idle"
 
 		//Elevator A2 "idle" at 1st Floor
-		b.columnList[0].elevatorList[1].currentFloor = 1
-		b.columnList[0].elevatorList[1].direction = "idle"
+		b.ColumnList[0].ElevatorList[1].CurrentFloor = 1
+		b.ColumnList[0].ElevatorList[1].Direction = "idle"
 
 		//Elevator A3 at B3rd, going "down" to B5th
-		b.columnList[0].elevatorList[2].currentFloor = -3
-		b.columnList[0].elevatorList[2].direction = "down"
+		b.ColumnList[0].ElevatorList[2].CurrentFloor = -3
+		b.ColumnList[0].ElevatorList[2].Direction = "down"
 
 		//Elevator A4 at B6th, going "up" to 1st
-		b.columnList[0].elevatorList[3].currentFloor = -6
-		b.columnList[0].elevatorList[3].direction = "up"
+		b.ColumnList[0].ElevatorList[3].CurrentFloor = -6
+		b.ColumnList[0].ElevatorList[3].Direction = "up"
 
 		//Elevator A5 at B1st, going "down" to b6th
-		b.columnList[0].elevatorList[4].currentFloor = -1
-		b.columnList[0].elevatorList[4].direction = "down"
+		b.ColumnList[0].ElevatorList[4].CurrentFloor = -1
+		b.ColumnList[0].ElevatorList[4].Direction = "down"
 
 		//1st, someone is at B3rd floor and requests the 1st floor, elevator A4 is expected to be sent
 		var numberFloor = -3
-		b.requestElevator(numberFloor)
+		b.RequestElevator(numberFloor)
 
 	}
 	retry()
